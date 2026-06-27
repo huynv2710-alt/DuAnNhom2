cff7f66fd5514151b6e167cf7faaab4877fc046a
longbtth07664-droid <longbtth07664@gmail.com>
2026-06-27 19:56:30 +0700

    huy gay

---
+++ src/main/java/contronler/quanlinhanvienServlet.java
@@ -0,0 +1,27 @@
+package contronler;
+
+import Models.NhanVien;
+import Service.quanlinhanvienservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/nhanvien")
+public class quanlinhanvienServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        quanlinhanvienservlet dao = new quanlinhanvienservlet();
+
+        ArrayList<NhanVien> list = dao.getAllNhanVien();
+
+        req.setAttribute("listNV", list);
+
+        req.getRequestDispatcher("QuanLyNhanVien.jsp").forward(req, resp);
+
+    }
+}
