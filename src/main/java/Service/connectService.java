e508d2a86eeda6ae3a665ca1037a06f0e856ef6f
longbtth07664-droid <longbtth07664@gmail.com>
2026-06-27 19:44:25 +0700

    long

--- src/main/java/Service/connectService.java
+++ src/main/java/Service/connectService.java
@@ -5,5 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class connectService {
+    private final String URL = "jdbc:sqlserver://localhost:1433;databaseName=JAVA101_FINAL_TEST;encrypt=true;trustServerCertificate=true";
+
+    private final String USER = "sa";
+
+    private final String PASSWORD = "123";
+
+    public connectService() {
+    }
+
+    public Connection myConnection(){
+        Connection conn = null;
+
+        try {
+
+            // Load Driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            // Create Connection
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Connect successful!");
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Driver not found!");
+            e.printStackTrace();
 
 }
