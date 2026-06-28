753f957744883d8e1d5bcdf305973b835e47b06e
longbtth07664-droid <longbtth07664@gmail.com>
2026-06-28 16:31:50 +0700

    bố long

--- src/main/java/controller/quanlinhanvienServlet.java
+++ src/main/java/controller/quanlinhanvienServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@WebServlet("/nhanvien")
+@WebServlet("/quanlinhanvien")
 public class quanlinhanvienServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
