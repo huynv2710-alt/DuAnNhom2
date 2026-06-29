3e5225162276f442546c9a02824c3ae7f22bf94d
longbtth07664-droid <longbtth07664@gmail.com>
2026-06-29 18:59:21 +0700

    huy gay

--- src/main/java/Service/quanlinhanvienservlet.java
+++ src/main/java/Service/quanlinhanvienservlet.java
@@ -90,7 +90,15 @@ public class quanlinhanvienservlet {
 
         connectService service = new connectService();
 
-        String sql = "SELECT * FROM NhanVien WHERE MaNV = ?";
+        String sql =
+                "SELECT nv.MaNV, nv.HoTen, nv.NgaySinh, nv.GioiTinh, " +
+                        "nv.SDT, nv.Email, nv.DiaChi, nv.MaTrangThai, " +
+                        "nv.CCCD, nv.NgayCapCCCD, nv.DacDiemNhanDang, " +
+                        "tt.TenTrangThai " +
+                        "FROM NhanVien nv " +
+                        "INNER JOIN TrangThaiNhanVien tt " +
+                        "ON nv.MaTrangThai = tt.MaTrangThai " +
+                        "WHERE nv.MaNV = ?";
 
         try (Connection conn = service.myConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -110,7 +118,7 @@ public class quanlinhanvienservlet {
                         rs.getString("Email"),
                         rs.getString("DiaChi"),
                         rs.getInt("MaTrangThai"),
-                        "",
+                        rs.getString("TenTrangThai"),
                         rs.getString("CCCD"),
                         rs.getDate("NgayCapCCCD"),
                         rs.getString("DacDiemNhanDang")
@@ -122,6 +130,46 @@ public class quanlinhanvienservlet {
         }
 
         return null;
+    }
+    public boolean update(NhanVien nv) {
+
+        connectService service = new connectService();
+
+        String sql = "UPDATE NhanVien SET "
+                + "HoTen=?, "
+                + "NgaySinh=?, "
+                + "GioiTinh=?, "
+                + "SDT=?, "
+                + "Email=?, "
+                + "DiaChi=?, "
+                + "MaTrangThai=?, "
+                + "CCCD=?, "
+                + "NgayCapCCCD=?, "
+                + "DacDiemNhanDang=? "
+                + "WHERE MaNV=?";
+
+        try (Connection conn = service.myConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, nv.getHoTen());
+            ps.setDate(2, nv.getNgaySinh());
+            ps.setString(3, nv.getGioiTinh());
+            ps.setString(4, nv.getSdt());
+            ps.setString(5, nv.getEmail());
+            ps.setString(6, nv.getDiaChi());
+            ps.setInt(7, nv.getMaTrangThai());
+            ps.setString(8, nv.getCccd());
+            ps.setDate(9, nv.getNgayCapCCCD());
+            ps.setString(10, nv.getDacDiemNhanDang());
+            ps.setInt(11, nv.getMaNV());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
     }
